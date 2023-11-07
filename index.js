86e6012c1fd45d539a00471d927665c2b5733230
Cole Peters <cole@colepeters.com>
2023-11-07 17:03:23 -0600

    Fix import paths

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import Masthead from 'begin-masthead.mjs'
-import BeginLogo from 'masthead-begin-logo.mjs'
-import DeployButton from 'masthead-deploy-button.mjs'
-import ProductLink from 'masthead-product-link.mjs'
-import SectionDropdown from 'masthead-section-dropdown.mjs'
-import SectionLink from 'masthead-section-link.mjs'
-import Slice from 'masthead-slice.mjs'
-import Symbols from 'masthead-symbols.mjs'
+import Masthead from './app/elements/begin-masthead.mjs'
+import BeginLogo from './app/elements/masthead-begin-logo.mjs'
+import DeployButton from './app/elements/masthead-deploy-button.mjs'
+import ProductLink from './app/elements/masthead-product-link.mjs'
+import SectionDropdown from './app/elements/masthead-section-dropdown.mjs'
+import SectionLink from './app/elements/masthead-section-link.mjs'
+import Slice from './app/elements/masthead-slice.mjs'
+import Symbols from './app/elements/masthead-symbols.mjs'
 
 export {
   Masthead,
